480602b0b686d00dd5278b60d11a499f9e8de1ba
Anna Kerps <anya-kerps@bk.ru>
2022-07-08 12:58:48 +0300

    Задача 36

---
+++ tasks/task36/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0  */ 
+
+int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if(i < array.Length-1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.Write("]");
+}
+
+int SumOfOddNumbers(int[] array)
+{
+    int sumOfArray = 0;
+    for (var i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 != 0)
+        {
+            sumOfArray += array[i];
+        }
+    }
+    return sumOfArray;
+}
+
+int [] NumbersArray = FillArray(4);
+PrintArray(NumbersArray);
+
+int OddSum = SumOfOddNumbers(NumbersArray);
+
+Console.Write($" -> {OddSum}");
